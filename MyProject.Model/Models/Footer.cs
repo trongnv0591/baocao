79c9287e59b86901789fdcec9467e4cd91a15315
TRONG-PC\Viet Trong <trongnv0591@gmail.com>
2021-06-14 17:13:07 +0700

    140621

---
+++ MyProject.Model/Models/Footer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyProject.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required]
+        public string Content { set; get; }
+    }
+}
---
+++ MyProject.Model/Models/MenuGroup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyProject.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public virtual IEnumerable<Menu> Menus { get; set; }
+    }
+}
